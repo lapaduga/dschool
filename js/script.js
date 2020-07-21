7926c8f90fc0760d144caa62ba1fe2c3cb5e2f7c
lapaduga <deniskotov29042015@gmail.com>
2020-07-22 02:20:28 +0500

    header done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
-	$('.header__school-burger, .close-menu-area').click(function(event){
+	$('.header__school-burger, .close-menu-area, .header__school-link').click(function(event){
 		$('.header__school-burger, .bg-for-menu, .header__school-menu').toggleClass('active');
 		$('body').toggleClass('lock');
 		$('.header__school-mobile-phone, .school-button').toggleClass('visibility');
-	});
-	$('.header__school-link').click(function(event){
-		$('.header__school-burger, .bg-for-menu, .header__school-menu').toggleClass('active');
-		$('body').toggleClass('lock');
 	});
 });
