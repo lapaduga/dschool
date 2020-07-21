f932dcef700ea27f888cb695a009ec3e331b53f2
lapaduga <deniskotov29042015@gmail.com>
2020-07-21 21:12:25 +0500

    new

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.header__school-burger').click(function(event){
+	$('.header__school-burger, .close-menu-area').click(function(event){
 		$('.header__school-burger, .bg-for-menu, .header__school-menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
